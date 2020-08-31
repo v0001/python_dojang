dba3d4bab85f95b68f68786fb8fcca616a055c2c
Vanessa Yoon <guswjd6005@gmail.com>
2020-08-31 22:09:32 +0900

    till unit 5

--- Easy EX/ex1.py
+++ Easy EX/ex1.py
@@ -1,1 +1,10 @@
 print('Hello World')
+
+print(int(3.3)) # 숫자
+
+print(divmod(5,2))
+
+a, b = divmod(7,3)
+print(a,b)
+
+
---
+++ Easy EX/ex2.py
@@ -0,0 +1,9 @@
+AP = 102
+result = AP*0.6+225
+
+print(result)
+
+
+a = float 10/3
+
+print(type(a))
